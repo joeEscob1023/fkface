753e8ac3ea8eadf8b5684e2b05ef813e9f276a52
Joe Escobedo <joe_escob1023@live.com>
2021-08-04 04:47:58 -0700

    links are working and cards look great

--- fkface/src/Components/Cast.js
+++ fkface/src/Components/Cast.js
@@ -1,17 +1,30 @@
 import React from "react";
+import {
+  Card, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
 
 const Cast = ({castArray}) => {
   console.log(castArray);
   return (
     <div>
-      <h1>{castArray.map((cast, id) => {
+      {castArray.map((cast, id) => {
         return (
-          <div key={id}>
-            {cast.image} : {cast.name}
-            
-          </div>
+          <Card key={id}>
+          <CardBody>
+            <CardTitle tag="h5">{cast.name}</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">{cast.status}</CardSubtitle>
+          </CardBody>
+          {/* <img width="100%" src={cast.image} alt="Card image cap" /> */}
+          {cast.image}
+          <CardBody>
+            <CardText>{cast.otherProductions}</CardText>
+            <CardLink href={`https://twitter.com/${cast.twitterHandle}`}>@{cast.twitterHandle}</CardLink>
+            <CardLink href={`https://www.instagram.com/${cast.instagramHandle}/?hl=en`}>@{cast.instagramHandle}</CardLink>
+          </CardBody>
+        </Card>
         )
-      })}</h1>
+      })}
     </div>
   );
 };
