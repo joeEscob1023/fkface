4fb25999d13cc1100c9d841537589e0e16d67887
Joe Escobedo <joe_escob1023@live.com>
2021-08-04 03:38:18 -0700

    routes working as intended, still getting weird error though

--- fkface/src/App.js
+++ fkface/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Cast from "./Components/Cast";
-
+import Home from './Components/Home'
 import Header from "./Components/Header";
 import ffCast from "./Data/Cast";
 
@@ -11,11 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div>
-        <h2>What is F**k Face?</h2>
-        <p>F**k Face is a weekly podcast that has the joining forces of Geoff Ramsey, Gavin Free and Andrew Panton</p>
-        <p>Coming to you live from Austin TX, every Wednesday, catch a new episode of these dip shits talk about the failures in their lives, mushrooms in their skinks and honey mustard.</p>
-      </div>
+      <Route exact path="/home" component={Home}/>
       <Switch>
         <Route path="/cast" 
         render={(props) => {
---
+++ fkface/src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+ return (
+
+ 
+    <div>
+        <h2>What is F**k Face?</h2>
+        <p>F**k Face is a weekly podcast that has the joining forces of Geoff Ramsey, Gavin Free and Andrew Panton</p>
+        <p>Coming to you live from Austin TX, every Wednesday, catch a new episode of these dip shits talk about the failures in their lives, mushrooms in their skinks and honey mustard.</p>
+      </div>
+ )
+ 
+}
+
+export default Home
