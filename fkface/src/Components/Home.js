97b4d06fe6c6b316c073111eb9783015c13d21d3
Joe Escobedo <joe_escob1023@live.com>
2021-08-19 20:43:52 -0700

    mobile friendly, now have to work on tablet

--- fkface/src/Components/Home.js
+++ fkface/src/Components/Home.js
@@ -4,19 +4,24 @@ const Home = () => {
   return (
     <div className="home">
       <h2>What is F**k Face?</h2>
-      <p>
-        F**k Face is a weekly podcast that has the joining forces of Geoff
-        Ramsey, Gavin Free and Andrew Panton.
-      </p>
-      <p>
-        Coming to you from Rooster Teeth Studios in Austin TX, every Wednesday,
-        catch a new episode of these goofs talk about the failures in their
-        lives, mushrooms in their skinks, honey mustard and a whole lot more!
-      </p>
-      <p>Enter the Zimmer Zone! ⚾️</p>
+      <hr className="hr" />
+      <div className="ff-info">
+        <p>
+          F**k Face is a weekly podcast that has the joining forces of Geoff
+          Ramsey, Gavin Free and Andrew Panton.
+        </p>
+        <p>
+          Coming to you from Rooster Teeth Studios in Austin TX, every
+          Wednesday, catch a new episode of these goofs talk about the failures
+          in their lives, mushrooms in their skinks, honey mustard and a whole
+          lot more!
+        </p>
+        <p>Enter the Zimmer Zone! ⚾️</p>
+      </div>
       <img
         src="https://lever-client-logos.s3.amazonaws.com/0bedc653-bcde-4011-a483-c393c6a83ce7-1494019829537.png"
         alt="rooster teeth logo"
+        className="rt-image"
       />
     </div>
   );
